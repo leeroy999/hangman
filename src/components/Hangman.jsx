484abad644c9e0f025bddd1f162cc5d1d0886797
leeroy999 <leeroy.whatsup@rocketmail.com>
2022-05-23 22:24:19 +0800

    Fix localstorage bug

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -24,7 +24,7 @@ const Hangman = ()=> {
 
   // When component mounts:
   useEffect(() => {
-    const newState = JSON.parse(localStorage.getItem("hangman") ?? "") ?? {};
+    const newState = localStorage.getItem("hangman") ? JSON.parse(localStorage.getItem("hangman")) : {};
     if (newState) {
       setState(newState);
     }
