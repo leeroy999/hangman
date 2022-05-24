78a88b6924e9fca7eff5c6759d1ff61056c3861e
Renn <reneeyeow02@gmail.com>
2022-05-24 14:58:28 +0800

    hangman animation change
    - change from prev jpgs to new pngs
    - background does not work yet but bg image is in src/assets folder

---
+++ src/components/HangmanAnimation.jsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from "react";
+import zero from "../assets/0.PNG";
+import one from "../assets/1.PNG";
+import two from "../assets/2.PNG";
+import three from "../assets/3.PNG";
+import four from "../assets/4.PNG";
+import five from "../assets/5.PNG";
+import six from "../assets/6.PNG";
+import HangmanContext from "./HangmanContext";
+
+const HangmanAnimation = () => {
+  const context = useContext(HangmanContext);
+  const [state, setState] = useState(zero);
+
+  useEffect(() => {
+    console.log(context.lives);
+    switch (context.lives) {
+      case 0:
+        setState(six);
+        break;
+      case 1:
+        setState(five);
+        break;
+      case 2:
+        setState(four);
+        break;
+      case 3:
+        setState(three);
+        break;
+      case 4:
+        setState(two);
+        break;
+      case 5:
+        setState(one);
+        break;
+      default:
+        setState(zero);
+    }
+  });
+
+  return (
+    <>
+      <img src={state} />
+    </>
+  );
+};
+
+export default HangmanAnimation;
